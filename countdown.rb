75aef9032513907a99fe6f670515a88f2c78eb57
Winter LaMon <winter.lamon@gmail.com>
2017-09-24 22:11:29 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-def countdown(seconds)
-  while seconds > 0
-    puts "#{seconds} SECOND(S)!"
-    seconds -= 1
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
   end
   puts "HAPPY NEW YEAR!"
 end
